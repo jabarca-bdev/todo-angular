38355c904065bdd1d8256ddf5b710be62e904e4c
Jose Abarca <jabarca@wiser.com>
2025-01-02 13:06:18 -0600

    add todo functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+
+interface TodoItem {
+  checked: boolean;
+  value: string;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'todo-angular';
+  todoInput = '';
+  todoList: TodoItem[] = [];
+
+  onClickTodoItem(todo: TodoItem) {
+    todo.checked = !todo.checked;
+  }
+
+  onDeleteTodoItem(index: number) {
+    console.log(index);
+    this.todoList.filter((item, idx) => idx !== index);
+  }
+
+  submit() {
+    console.log(this.todoInput);
+
+    if (this.todoInput.length) {
+      this.todoList.push({
+        checked: false,
+        value: this.todoInput,
+      });
+      this.todoInput = '';
+    }
+  }
 }
